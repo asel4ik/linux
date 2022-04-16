577a47b0602636d49b30e9010249c9804d922e18
Adam Skladowski <a_skl39@protonmail.com>
2022-04-16 19:09:15 +0200

    hfpll:add MSM8976 data.

--- drivers/clk/qcom/hfpll.c
+++ drivers/clk/qcom/hfpll.c
@@ -6,6 +6,7 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/clk.h>
 #include <linux/clk-provider.h>
 #include <linux/regmap.h>
@@ -31,8 +32,85 @@ static const struct hfpll_data hdata = {
 	.max_rate = 2900000000UL,
 };
 
+static const struct hfpll_data msm8976_a53_hdata = {
+	.mode_reg = 0x0,
+	.l_reg = 0x4,
+	.m_reg = 0x8,
+	.n_reg = 0xC,
+	.user_reg = 0x10,
+	.config_reg = 0x14,
+	.status_reg = 0x1C,
+	.lock_bit = 16,
+	.user_vco_mask = 0x3 << 20,
+	.pre_div_mask = 0x7 << 12,
+	.post_div_mask = (BIT(8) | BIT(9)),
+	.post_div_masked =  0x1 << 8,
+	.early_output_mask =  BIT(3),
+	.main_output_mask = BIT(0),
+	.config_val = 0x00341600,
+	.user_vco_val = 0x00141400,
+	.vco_mode_masked = BIT(20),
+	.min_rate = 652800000UL,
+	.max_rate = 1478400000UL,
+	.low_vco_max_rate = 902400000UL,
+	.l_val = 0x49,
+	.safe_parking_enabled = false,
+	
+};
+static const struct hfpll_data msm8976_a72_hdata = {
+	.mode_reg = 0x0,
+	.l_reg = 0x4,
+	.m_reg = 0x8,
+	.n_reg = 0xC,
+	.user_reg = 0x10,
+	.config_reg = 0x14,
+	.status_reg = 0x1C,
+	.lock_bit = 16,
+	.user_vco_mask = 0x3 << 28,
+	.pre_div_mask = BIT(12),
+	.pre_div_masked = 0,
+	.post_div_mask = (BIT(8) | BIT(9)),
+	.post_div_masked = 0x100,
+	.early_output_mask =  0x8,
+	.main_output_mask = BIT(0),
+	.vco_mode_masked = BIT(20),
+	.config_val = 0x04E0405D,
+	.max_rate = 2016000000UL,
+	.min_rate = 940800000UL,
+	.l_val = 0x5B,
+	.l_park_val = 0x35,
+	.safe_parking_enabled = true,
+};
+
+static const struct hfpll_data msm8976_cci_hdata = {
+	.mode_reg = 0x0,
+	.l_reg = 0x4,
+	.m_reg = 0x8,
+	.n_reg = 0xC,
+	.user_reg = 0x10,
+	.config_reg = 0x14,
+	.status_reg = 0x1C,
+	.lock_bit = 16,
+	.user_vco_mask = 0x3 << 20,
+	.pre_div_mask = 0x7 << 12,
+	.post_div_mask = (BIT(8) | BIT(9)),
+	.early_output_mask =  BIT(3),
+	.main_output_mask = BIT(0),
+	.post_div_masked = 0x1 << 8,
+	.vco_mode_masked = BIT(20),
+	.config_val = 0x00141400,
+	.min_rate = 307200000UL,
+	.max_rate = 902400000UL,
+	.l_val = 0x20,
+	.safe_parking_enabled = false,
+};
+
 static const struct of_device_id qcom_hfpll_match_table[] = {
-	{ .compatible = "qcom,hfpll" },
+	{ .compatible = "qcom,hfpll", &hdata },
+	{ .compatible = "qcom,hfpll2" },
+	{ .compatible = "qcom,hfpll-msm8976-a53", &msm8976_a53_hdata },
+	{ .compatible = "qcom,hfpll-msm8976-a72", &msm8976_a72_hdata },
+	{ .compatible = "qcom,hfpll-msm8976-cci", &msm8976_cci_hdata },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, qcom_hfpll_match_table);
@@ -51,6 +129,7 @@ static int qcom_hfpll_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	void __iomem *base;
 	struct regmap *regmap;
+	struct hfpll_data *pll_data;
 	struct clk_hfpll *h;
 	struct clk_init_data init = {
 		.num_parents = 1,
@@ -63,6 +142,12 @@ static int qcom_hfpll_probe(struct platform_device *pdev)
 		 */
 		.flags = CLK_IGNORE_UNUSED,
 	};
+	pll_data = of_device_get_match_data(&pdev->dev);
+	
+	if (of_device_is_compatible(pdev->dev.of_node, "qcom,hfpll2")) {
+	init.ops = &clk_ops_hf2_pll;
+	}
+	
 	int ret;
 	struct clk_parent_data pdata = { .index = 0 };
 
@@ -85,7 +170,8 @@ static int qcom_hfpll_probe(struct platform_device *pdev)
 
 	init.parent_data = &pdata;
 
-	h->d = &hdata;
+
+	h->d = pll_data;
 	h->clkr.hw.init = &init;
 	spin_lock_init(&h->lock);
 

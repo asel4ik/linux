be0b69bdaa143bc2eca68fe4a69dea95ddd72a5d
Adam Skladowski <a_skl39@protonmail.com>
2022-05-08 13:57:34 +0200

    phy-qcom-usb-hs28nm:upgade driver.

--- drivers/phy/qualcomm/phy-qcom-usb-hs-28nm.c
+++ drivers/phy/qualcomm/phy-qcom-usb-hs-28nm.c
@@ -16,18 +16,65 @@
 #include <linux/regulator/consumer.h>
 #include <linux/reset.h>
 #include <linux/slab.h>
+#include <linux/regmap.h>
+
 
 /* PHY register and bit definitions */
-#define PHY_CTRL_COMMON0		0x078
-#define SIDDQ				BIT(2)
-#define PHY_IRQ_CMD			0x0d0
+
+#define USB_PHY_UTMI_CTRL5		0x0074
+#define PICOPHY_UTMI_CONTROL_OVERWRITE_EN	BIT(4)
+#define POR					BIT(1)
+#define ATERESET				BIT(0)
+#define USB_PHY_HS_PHY_CTRL_COMMON0	0x0078
+#define COMMONONN				BIT(7)
+#define FSEL					GENMASK(6, 4)
+#define RETENABLEN				BIT(3)
+#define SIDDQ					BIT(2)
+#define VATESTENB				GENMASK(1, 0)
+#define USB_PHY_HS_PHY_CTRL_COMMON1	0x007c
+#define PLLBTUNE				BIT(7)
+#define USE_CLKCORE				BIT(6)
+#define ACAENB0					BIT(5)
+#define VBUSVLDEXTSEL0				BIT(4)
+#define OTGDISABLE0				BIT(3)
+#define OTGTUNE0				GENMASK(2, 0)
+#define USB_PHY_HS_PHY_CTRL1		0x008c
+#define VBUSVLDEXT0				BIT(5)
+#define ULPIPOR					BIT(2)
+#define ID_HV_CLAMP_EN_N			BIT(1)
+#define ULPI_CLK_EN				BIT(0)
+#define USB_PHY_HS_PHY_CTRL2		0x0090
+#define USB2_SUSPEND_N_SEL			BIT(7)
+#define USB2_SUSPEND_N				BIT(6)
+#define USB2_UTMI_CLK_EN			BIT(5)
+#define AUTORESUME_LEGACY			BIT(4)
+#define AUTORESUME				BIT(3)
+#define USB_PHY_PARAMETER_OVERRIDE_X0	0x0098
+#define SQRXTUNE0				GENMASK(7, 5)
+#define COMPDISTUNE0				GENMASK(2, 0)
+#define USB_PHY_PARAMETER_OVERRIDE_X1	0x009c
+#define TXPREEMPAMPTUNE0			GENMASK(7, 6)
+#define TXPREEMPPULSETUNE0			BIT(5)
+#define TXVREFTUNE0				GENMASK(3, 0)
+#define USB_PHY_PARAMETER_OVERRIDE_X2		0x00A0
+#define TXRESTUNE0				GENMASK(5, 4)
+#define TXHSXVTUNE0				GENMASK(3, 2)
+#define TXRISETUNE0				GENMASK(1, 0)
+#define USB_PHY_PARAMETER_OVERRIDE_X3	0x00a4
+#define TXFSLSTUNE0				GENMASK(3, 0)
+#define USB_PHY_TCSR_CTRL		0x00c0
+#define SNPS_CTRL_USB2_PHY			BIT(0)
+#define USB_PHY_IRQ_CMD			0x0d0
+#define IRQ_GLOBAL_CLEAR			BIT(0)
 #define PHY_INTR_MASK0			0x0d4
 #define PHY_INTR_CLEAR0			0x0dc
-#define DPDM_MASK			0x1e
-#define DP_1_0				BIT(4)
-#define DP_0_1				BIT(3)
-#define DM_1_0				BIT(2)
-#define DM_0_1				BIT(1)
+#define DP_1_0					BIT(4)
+#define DP_0_1					BIT(3)
+#define DM_1_0					BIT(2)
+#define DM_0_1					BIT(1)
+#define USB_PHY_REFCLK_CTRL		0x00e8
+#define REFCLK_SEL				GENMASK(3, 1)
+#define REFCLK_RXTAP_EN				BIT(0)
 
 enum hsphy_voltage {
 	VOL_NONE,
@@ -54,6 +101,35 @@ struct hsphy_data {
 	unsigned int init_seq_num;
 };
 
+/* struct override_param - structure holding one femtophy overriding param
+ * set override true if the  device tree property exists and read and assign
+ * to value
+ */
+struct override_param {
+	bool override;
+	u8 value;
+};
+
+/* struct override_params - structure holding overriding params
+ * for femtophy strap signals exposed in registers
+ *
+ * currently only straps overriden during the qcs405 init sequence are present
+ *
+ * @snps_ctrl_usb2_phy: not actually a femtophy strap signal, but is set as
+ *  part of the qcs405 init sequence; meaning unknown
+ * @commononn: Common Block Power-Down Control, set low to prevent CLK48MOHCI,
+ *  CLK12MOHCI, and CLK480M from powering down in suspend/sleep.
+ * @otgtune0: set lower or higher than 0x3 to adjust VBUS Valid Threshold
+ * @usb2_utmi_clk_en: not a name of a femtophy strap signal, why qcom why...
+ */
+struct override_params {
+	struct override_param snps_ctrl_usb2_phy;
+	struct override_param commononn;
+	struct override_param otgtune0;
+	struct override_param usb2_utmi_clk_en;
+	bool overrides_present;
+};
+
 struct hsphy_priv {
 	void __iomem *base;
 	struct clk_bulk_data *clks;
@@ -63,6 +139,16 @@ struct hsphy_priv {
 	struct regulator_bulk_data vregs[VREG_NUM];
 	const struct hsphy_data *data;
 	enum phy_mode mode;
+	struct regmap *map;
+	struct override_params overrides;
+};
+
+static const struct regmap_config phy_regmap_cfg = {
+	.reg_bits = 32,
+	.val_bits = 8,	/* the register is 32 bits long, but really contains four copies */
+			/* of the actually implemented lower 8 bits                      */
+	.reg_stride = 4,
+	.max_register = 0x200,
 };
 
 static int qcom_snps_hsphy_set_mode(struct phy *phy, enum phy_mode mode,
@@ -78,78 +164,57 @@ static int qcom_snps_hsphy_set_mode(struct phy *phy, enum phy_mode mode,
 	return 0;
 }
 
-static void qcom_snps_hsphy_enable_hv_interrupts(struct hsphy_priv *priv)
+static inline void qcom_snps_hsphy_clear_pending_interrupts(struct hsphy_priv *priv)
 {
-	u32 val;
+	regmap_set_bits(priv->map, PHY_INTR_CLEAR0, DP_1_0 | DP_0_1 | DM_1_0 | DM_0_1);
 
-	/* Clear any existing interrupts before enabling the interrupts */
-	val = readb(priv->base + PHY_INTR_CLEAR0);
-	val |= DPDM_MASK;
-	writeb(val, priv->base + PHY_INTR_CLEAR0);
-
-	writeb(0x0, priv->base + PHY_IRQ_CMD);
+	regmap_clear_bits(priv->map, USB_PHY_IRQ_CMD, IRQ_GLOBAL_CLEAR);
 	usleep_range(200, 220);
-	writeb(0x1, priv->base + PHY_IRQ_CMD);
+	regmap_set_bits(priv->map, USB_PHY_IRQ_CMD, IRQ_GLOBAL_CLEAR);
 
 	/* Make sure the interrupts are cleared */
 	usleep_range(200, 220);
+}
 
-	val = readb(priv->base + PHY_INTR_MASK0);
+static void qcom_snps_hsphy_enable_hv_interrupts(struct hsphy_priv *priv)
+{
+	/* Clear any existing interrupts before enabling the interrupts */
+	qcom_snps_hsphy_clear_pending_interrupts(priv);
+
 	switch (priv->mode) {
 	case PHY_MODE_USB_HOST_HS:
 	case PHY_MODE_USB_HOST_FS:
 	case PHY_MODE_USB_DEVICE_HS:
 	case PHY_MODE_USB_DEVICE_FS:
-		val |= DP_1_0 | DM_0_1;
+		regmap_set_bits(priv->map, PHY_INTR_MASK0, DP_1_0 | DM_0_1);
 		break;
 	case PHY_MODE_USB_HOST_LS:
 	case PHY_MODE_USB_DEVICE_LS:
-		val |= DP_0_1 | DM_1_0;
+		regmap_set_bits(priv->map, PHY_INTR_MASK0, DP_0_1 | DM_1_0);
 		break;
 	default:
 		/* No device connected */
-		val |= DP_0_1 | DM_0_1;
+		regmap_set_bits(priv->map, PHY_INTR_MASK0, DP_0_1 | DM_0_1);
 		break;
 	}
-	writeb(val, priv->base + PHY_INTR_MASK0);
 }
 
-static void qcom_snps_hsphy_disable_hv_interrupts(struct hsphy_priv *priv)
+static inline void qcom_snps_hsphy_disable_hv_interrupts(struct hsphy_priv *priv)
 {
-	u32 val;
-
-	val = readb(priv->base + PHY_INTR_MASK0);
-	val &= ~DPDM_MASK;
-	writeb(val, priv->base + PHY_INTR_MASK0);
+	regmap_clear_bits(priv->map, PHY_INTR_MASK0, DP_1_0 | DP_0_1 | DM_1_0 | DM_0_1);
 
 	/* Clear any pending interrupts */
-	val = readb(priv->base + PHY_INTR_CLEAR0);
-	val |= DPDM_MASK;
-	writeb(val, priv->base + PHY_INTR_CLEAR0);
-
-	writeb(0x0, priv->base + PHY_IRQ_CMD);
-	usleep_range(200, 220);
-
-	writeb(0x1, priv->base + PHY_IRQ_CMD);
-	usleep_range(200, 220);
+	qcom_snps_hsphy_clear_pending_interrupts(priv);
 }
 
-static void qcom_snps_hsphy_enter_retention(struct hsphy_priv *priv)
+static inline void qcom_snps_hsphy_enter_retention(struct hsphy_priv *priv)
 {
-	u32 val;
-
-	val = readb(priv->base + PHY_CTRL_COMMON0);
-	val |= SIDDQ;
-	writeb(val, priv->base + PHY_CTRL_COMMON0);
+	regmap_set_bits(priv->map, USB_PHY_HS_PHY_CTRL_COMMON0, SIDDQ);
 }
 
-static void qcom_snps_hsphy_exit_retention(struct hsphy_priv *priv)
+static inline void qcom_snps_hsphy_exit_retention(struct hsphy_priv *priv)
 {
-	u32 val;
-
-	val = readb(priv->base + PHY_CTRL_COMMON0);
-	val &= ~SIDDQ;
-	writeb(val, priv->base + PHY_CTRL_COMMON0);
+	regmap_clear_bits(priv->map, USB_PHY_HS_PHY_CTRL_COMMON0, SIDDQ);
 }
 
 static int qcom_snps_hsphy_power_on(struct phy *phy)
@@ -197,23 +262,35 @@ static int qcom_snps_hsphy_reset(struct hsphy_priv *priv)
 	return 0;
 }
 
-static void qcom_snps_hsphy_init_sequence(struct hsphy_priv *priv)
+static void qcom_snps_hsphy_set_overrides(struct hsphy_priv *priv)
 {
-	const struct hsphy_data *data = priv->data;
-	const struct hsphy_init_seq *seq;
-	int i;
+	struct override_params *or = &priv->overrides;
 
-	/* Device match data is optional. */
-	if (!data)
+	// don't needlessly reset if we're not applying overrides
+	if(!or->overrides_present)
 		return;
 
-	seq = data->init_seq;
+	/* the reason for why some overrides are applied before asserting POR is not known */
+	if(or->snps_ctrl_usb2_phy.override)
+		regmap_update_bits(priv->map, USB_PHY_TCSR_CTRL, SNPS_CTRL_USB2_PHY, or->snps_ctrl_usb2_phy.value);
 
-	for (i = 0; i < data->init_seq_num; i++, seq++) {
-		writeb(seq->val, priv->base + seq->offset);
-		if (seq->delay)
-			usleep_range(seq->delay, seq->delay + 10);
-	}
+	/* assert POR (relation to the GCC-controller POR signal unknown) */
+	regmap_set_bits(priv->map, USB_PHY_UTMI_CTRL5, POR);
+
+	if(or->commononn.override)
+		regmap_update_bits(priv->map, USB_PHY_HS_PHY_CTRL_COMMON0, COMMONONN, or->commononn.value);
+
+	if(or->otgtune0.override)
+		regmap_update_bits(priv->map, USB_PHY_HS_PHY_CTRL_COMMON1, OTGTUNE0, or->otgtune0.value);
+
+	if(or->usb2_utmi_clk_en.override)
+		regmap_update_bits(priv->map, USB_PHY_HS_PHY_CTRL2, USB2_UTMI_CLK_EN, or->usb2_utmi_clk_en.value);
+
+	/* de-assert POR */
+	regmap_set_bits(priv->map, USB_PHY_HS_PHY_CTRL2, USB2_SUSPEND_N_SEL);
+	regmap_clear_bits(priv->map, USB_PHY_UTMI_CTRL5, POR);
+	regmap_clear_bits(priv->map, USB_PHY_HS_PHY_CTRL2, USB2_SUSPEND_N_SEL);
+
 }
 
 static int qcom_snps_hsphy_por_reset(struct hsphy_priv *priv)
@@ -266,7 +343,7 @@ static int qcom_snps_hsphy_init(struct phy *phy)
 	if (ret)
 		goto disable_clocks;
 
-	qcom_snps_hsphy_init_sequence(priv);
+	qcom_snps_hsphy_set_overrides(priv);
 
 	ret = qcom_snps_hsphy_por_reset(priv);
 	if (ret)
@@ -311,6 +388,7 @@ static int qcom_snps_hsphy_probe(struct platform_device *pdev)
 	struct phy *phy;
 	int ret;
 	int i;
+	u32 value;
 
 	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
 	if (!priv)
@@ -319,6 +397,8 @@ static int qcom_snps_hsphy_probe(struct platform_device *pdev)
 	priv->base = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(priv->base))
 		return PTR_ERR(priv->base);
+
+	priv->map  = devm_regmap_init_mmio(dev, priv->base , &phy_regmap_cfg);
 
 	priv->num_clks = ARRAY_SIZE(qcom_snps_hsphy_clks);
 	priv->clks = devm_kcalloc(dev, priv->num_clks, sizeof(*priv->clks),
@@ -352,6 +432,32 @@ static int qcom_snps_hsphy_probe(struct platform_device *pdev)
 	/* Get device match data */
 	priv->data = device_get_match_data(dev);
 
+	/* Overwrite settings */
+
+ 	if (!of_property_read_u32(dev->of_node, "qcom,snps_ctrl_usb2_phy-value", &value)) {
+		priv->overrides.snps_ctrl_usb2_phy.value = (u8)value;
+		priv->overrides.snps_ctrl_usb2_phy.override = true;
+		priv->overrides.overrides_present = true;
+	}
+
+	if (!of_property_read_u32(dev->of_node, "qcom,commononn-value", &value)) {
+		priv->overrides.commononn.value = (u8)value;
+		priv->overrides.commononn.override = true;
+		priv->overrides.overrides_present = true;
+	}
+
+	if (!of_property_read_u32(dev->of_node, "qcom,otgtune0-value", &value)) {
+		priv->overrides.otgtune0.value = (u8)value;
+		priv->overrides.otgtune0.override = true;
+		priv->overrides.overrides_present = true;
+	}
+
+	if (!of_property_read_u32(dev->of_node, "qcom,usb2_utmi_clk_en-value", &value)) {
+		priv->overrides.usb2_utmi_clk_en.value = (u8)value;
+		priv->overrides.usb2_utmi_clk_en.override = true;
+		priv->overrides.overrides_present = true;
+	}
+
 	phy = devm_phy_create(dev, dev->of_node, &qcom_snps_hsphy_ops);
 	if (IS_ERR(phy))
 		return PTR_ERR(phy);
@@ -378,50 +484,8 @@ unset_1p8_load:
 	return ret;
 }
 
-/*
- * The macro is used to define an initialization sequence.  Each tuple
- * is meant to program 'value' into phy register at 'offset' with 'delay'
- * in us followed.
- */
-#define HSPHY_INIT_CFG(o, v, d)	{ .offset = o, .val = v, .delay = d, }
-
-static const struct hsphy_init_seq init_seq_femtophy[] = {
-	HSPHY_INIT_CFG(0xc0, 0x01, 0),
-	HSPHY_INIT_CFG(0xe8, 0x0d, 0),
-	HSPHY_INIT_CFG(0x74, 0x12, 0),
-	HSPHY_INIT_CFG(0x98, 0x63, 0),
-	HSPHY_INIT_CFG(0x9c, 0x03, 0),
-	HSPHY_INIT_CFG(0xa0, 0x1d, 0),
-	HSPHY_INIT_CFG(0xa4, 0x03, 0),
-	HSPHY_INIT_CFG(0x8c, 0x23, 0),
-	HSPHY_INIT_CFG(0x78, 0x08, 0),
-	HSPHY_INIT_CFG(0x7c, 0xdc, 0),
-	HSPHY_INIT_CFG(0x90, 0xe0, 20),
-	HSPHY_INIT_CFG(0x74, 0x10, 0),
-	HSPHY_INIT_CFG(0x90, 0x60, 0),
-};
-
-static const struct hsphy_init_seq init_seq_mdm9607[] = {
-	HSPHY_INIT_CFG(0x80, 0x44, 0),
-	HSPHY_INIT_CFG(0x81, 0x38, 0),
-	HSPHY_INIT_CFG(0x82, 0x24, 0),
-	HSPHY_INIT_CFG(0x83, 0x13, 0),
-};
-
-static const struct hsphy_data hsphy_data_femtophy = {
-	.init_seq = init_seq_femtophy,
-	.init_seq_num = ARRAY_SIZE(init_seq_femtophy),
-};
-
-static const struct hsphy_data hsphy_data_mdm9607 = {
-	.init_seq = init_seq_mdm9607,
-	.init_seq_num = ARRAY_SIZE(init_seq_mdm9607),
-};
-
 static const struct of_device_id qcom_snps_hsphy_match[] = {
-	{ .compatible = "qcom,usb-hs-28nm-femtophy", .data = &hsphy_data_femtophy, },
-	{ .compatible = "qcom,usb-hs-28nm-femtophy-generic", },
-	{ .compatible = "qcom,usb-hs-28nm-mdm9607", .data = &hsphy_data_mdm9607, },
+	{ .compatible = "qcom,usb-hs-28nm-femtophy" },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, qcom_snps_hsphy_match);
@@ -437,3 +501,4 @@ module_platform_driver(qcom_snps_hsphy_driver);
 
 MODULE_DESCRIPTION("Qualcomm 28nm Hi-Speed USB PHY driver");
 MODULE_LICENSE("GPL v2");
+

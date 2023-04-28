cc0075c03fdd437aa1a07bee1be907a19f3e23ce
Adam Skladowski <a39.skl@gmail.com>
2023-04-28 18:51:29 +0200

    dt-bindings: interconnect: Add Qualcomm MDM9607 DT bindings
    Add bindings for Qualcomm MDM9607 Network-On-Chip interconnect devices.

---
+++ include/dt-bindings/interconnect/qcom,mdm9607.h
@@ -0,0 +1,67 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Qualcomm MDM9607 interconnect IDs
+ */
+
+#ifndef __DT_BINDINGS_INTERCONNECT_QCOM_MDM9607_H
+#define __DT_BINDINGS_INTERCONNECT_QCOM_MDM9607_H
+
+/* BIMC fabric */
+#define MAS_APPS_PROC 		        0
+#define MAS_PCNOC_BIMC_1 		1
+#define MAS_TCU_0 			2
+#define SLV_EBI 			3
+#define SLV_BIMC_PCNOC 		        4
+
+/* PCNOC fabric */
+#define MAS_QDSS_BAM 			0
+#define MAS_BIMC_PCNOC                  1
+#define MAS_QDSS_ETR 			2
+#define MAS_AUDIO 			3
+#define MAS_QPIC 			4
+#define MAS_HSIC 			5
+#define MAS_BLSP_1 			6
+#define MAS_USB_HS1 			7
+#define MAS_CRYPTO 			8
+#define MAS_SDCC_1 			9
+#define MAS_SDCC_2 			10
+#define MAS_XI_USB_HS1                  11
+#define MAS_XI_HSIC 			12
+#define MAS_SGMII 			13
+#define PCNOC_M_0 			14
+#define PCNOC_M_1 			15
+#define QDSS_INT 			16
+#define PCNOC_INT_0 			17
+#define PCNOC_INT_2 			18
+#define PCNOC_INT_3 			19
+#define PCNOC_S_0 			20
+#define PCNOC_S_1 			21
+#define PCNOC_S_2 			22
+#define PCNOC_S_3 			23
+#define PCNOC_S_4 			24
+#define PCNOC_S_5 			25
+#define SLV_PCNOC_BIMC_1 		26
+#define SLV_QDSS_STM 			27
+#define SLV_CATS_0 			28
+#define SLV_IMEM 			29
+#define SLV_TCSR 			30
+#define SLV_SDCC_1 			31
+#define SLV_BLSP_1 			32
+#define SLV_SGMII 			33
+#define SLV_CRYPTO_0_CFG 		34
+#define SLV_MESSAGE_RAM 		35
+#define SLV_PDM 			36
+#define SLV_PRNG 			37
+#define SLV_USB2 			38
+#define SLV_SDCC_2 			39
+#define SLV_AUDIO 			40
+#define SLV_HSIC 			41
+#define SLV_USB_PHY 			42
+#define SLV_TLMM 			43
+#define SLV_IMEM_CFG 			44
+#define SLV_PMIC_ARB 			45
+#define SLV_TCU 			46
+#define SLV_QIPC 			47
+
+
+#endif /* __DT_BINDINGS_INTERCONNECT_QCOM_MDM9607_H */

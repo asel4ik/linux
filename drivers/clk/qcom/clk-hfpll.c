17a5d0fbb695a1c70d11281f967548a6c8a0df16
Adam Skladowski <a39.skl@gmail.com>
2023-07-15 18:14:18 +0200

    !fixup L val

--- drivers/clk/qcom/clk-hfpll.c
+++ drivers/clk/qcom/clk-hfpll.c
@@ -44,7 +44,7 @@ static void __clk_hfpll_init_once(struct clk_hw *hw)
 		regmap_write(regmap, hd->user_reg, regval);
 	}
 
-          /* Write L_VAL from conf if it exist */
+          /* Write L_VAL from conf if it exists */
         if (hd->l_val)
 	      regmap_write(regmap, hd->l_reg, hd->l_val);
 
